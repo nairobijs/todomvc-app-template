5d6b4df8a52614d0673973746c3060aebcac852c
jheneknights <eugenemutai@gmail.com>
2015-05-02 17:09:21 +0300

    added assets so as not to need node_modules

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
-(function (window) {
-	'use strict';
+ var TodoApp = angular.module('TodoApp', []);
 
-	// Your starting point. Enjoy the ride!
+ TodoApp.controller('TodoAppController', ['$scope', TodoAppController]);
+ TodoApp.controller('TodoController', ['$scope', TodoController]);
 
-})(window);
+ // The main app controller, bigger than the one 
+ // below it since it wraps also the DOM belonging to the controllers below it
+ function TodoAppController($scope) {
+     $scope.app_title = "Todo app list"
+ }
+
+ // Adding the modules as arguments that
+ // have been injected above in the controller
+ function TodoController($scope) {
+     $scope.todo_list = [{
+         is_done: false,
+         task: "Dance in Honolulu"
+     }, {
+         is_done: false,
+         task: "Watch Manny beat Mayweather"
+     }, {
+         is_done: true,
+         task: "Go to shaping up with Angularjs"
+     }, {
+         is_done: false,
+         task: "Get a girlfriend for once in my life"
+     }, {
+         is_done: false,
+         task: "Take a shower too"
+     }];
+
+     $scope.setTask = function (is_done, $index) {
+         console.log("IS THE TASK DONE?", is_done, $index);
+
+         // check if the task is done
+         // if it was done make it undone
+         // and viceversa
+         var done = is_done == true ? false : true;
+         $scope.todo_list[$index].is_done = done;
+     }
+ }
